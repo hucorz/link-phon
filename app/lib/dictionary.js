e16f624d45c03b9f7db7652e99b76977f60910ed
hucorz <chaohui6014@gmail.com>
2024-03-02 21:39:44 +0800

    update

--- app/lib/dictionary.js
+++ app/lib/dictionary.js
@@ -3,8 +3,5 @@ export const lookupPhonUS = async (word) => {
   word = word.trim().toLowerCase();
   const response = await fetch(`http://localhost:5328/api/dict/phon_us?q=${word}`);
   const phonUs = await response.json();
-  console.log(1111);
-  console.log(phonUs);
-  console.log(1111);
   return phonUs.phon_us;
 };
