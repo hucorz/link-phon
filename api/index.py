8fa3a634a9bb3ca45406aa5beec5f6452925e692
hucorz <chaohui6014@gmail.com>
2024-03-02 20:39:44 +0800

    init

--- api/index.py
+++ api/index.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from readmdict import MDX
+from bs4 import BeautifulSoup
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+filename = "OxfordAdvancedDictionary(10thEdition)V3.mdx"
+headwords = [*MDX(filename)]  # 单词名列表
+items = [*MDX(filename).items()]  # 释义html源码列表
+
+def lookup_phon_us(word):
+    word = word.strip().lower()
+    wordIndex = headwords.index(word.encode())
+    word, html = items[wordIndex]
+    word, html = word.decode(), html.decode()
+
+    soup = BeautifulSoup(html, "html.parser")
+    phon_us = soup.find(class_='top-container').find(class_='phons_n_am').find(class_='phon')
+
+    assert phon_us is not None, "No phonetic symbol found"
+    return phon_us.text[1:-1]
+
+# Path: /api/dict/phon_us?q=<word>
+# Example: /api/dict/phon_us?q=word
+@app.route("/api/dict/phon_us")
+def dict_phon_us():
+    word = request.args.get('q')
+    phon_us = lookup_phon_us(word)
+    return jsonify({"phon_us": phon_us})
--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,167 @@
-import Image from 'next/image'
-import Link from 'next/link'
+'use client';
 
-export default function Home() {
+import { NextUIProvider, Input, Card, CardBody } from '@nextui-org/react';
+import { useRef, useState } from 'react';
+import { lookupPhonUS } from './lib/dictionary';
+import { Phoneme, splitPhoneme } from './lib/phon';
+import { ifLinkPhon } from './lib/linkPhon';
+
+const Page = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  let wordPhons: Phoneme[][] = [];
+  const [showPhon, setShowPhon] = useState<JSX.Element[]>([]);
+  const [showWord, setShowWord] = useState<JSX.Element[]>([]);
+
+  const getPhonString = (phon: Phoneme[]) => {
+    let str = '';
+    for (let p of phon) {
+      str += p.ipa;
+    }
+    return str;
+  };
+
+  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      if (inputRef.current) {
+        wordPhons = [];
+        setShowPhon([]);
+        setShowWord([]);
+
+        const sentence = inputRef.current.value;
+        const words = sentence.trim().split(' ');
+        for (let word of words) {
+          const phon = await lookupPhonUS(word);
+          wordPhons.push(splitPhoneme(phon));
+        }
+
+        let isLink: boolean[] = [];
+        let sp: JSX.Element[] = [];
+        let sw: JSX.Element[] = [];
+
+        for (let i = 0; i < wordPhons.length - 1; i++) {
+          isLink.push(ifLinkPhon(wordPhons[i], wordPhons[i + 1]));
+        }
+        isLink.push(false);
+
+        // set showWord
+        for (let i = 0; i < words.length; i++) {
+          if (words[i].length === 1) {
+            sw.push(
+              <span
+                className={
+                  (i > 0 && (isLink[i - 1] || isLink[i])) ||
+                  (i === 0 && isLink[i])
+                    ? 'underline'
+                    : ''
+                }
+                key={sw.length}
+              >
+                {words[i]}
+              </span>,
+            );
+          } else {
+            sw.push(
+              <span
+                className={i > 0 && isLink[i - 1] ? 'underline' : ''}
+                key={sw.length}
+              >
+                {words[i][0]}
+              </span>,
+            );
+            sw.push(<span key={sw.length}>{words[i].slice(1, -1)}</span>);
+            sw.push(
+              <span className={isLink[i] ? 'underline' : ''} key={sw.length}>
+                {words[i][words[i].length - 1]}
+              </span>,
+            );
+            sw.push(
+              <span
+                className={i < words.length - 1 && isLink[i] ? 'underline' : ''}
+                key={sw.length}
+              >
+                {' '}
+              </span>,
+            );
+          }
+        }
+        setShowWord(sw);
+
+        // set showPhon
+        for (let i = 0; i < words.length; i++) {
+          if (wordPhons[i].length === 1) {
+            sp.push(
+              <span
+                className={
+                  (i > 0 && (isLink[i - 1] || isLink[i])) ||
+                  (i === 0 && isLink[i])
+                    ? 'underline'
+                    : ''
+                }
+                key={sp.length}
+              >
+                {getPhonString(wordPhons[i])}
+              </span>,
+            );
+          } else {
+            sp.push(
+              <span
+                className={i > 0 && isLink[i - 1] ? 'underline' : ''}
+                key={sp.length}
+              >
+                {getPhonString(wordPhons[i].slice(0, 1))}
+              </span>,
+            );
+            sp.push(
+              <span key={sp.length}>
+                {getPhonString(wordPhons[i].slice(1, -1))}
+              </span>,
+            );
+            sp.push(
+              <span className={isLink[i] ? 'underline' : ''} key={sp.length}>
+                {getPhonString(wordPhons[i].slice(-1))}
+              </span>,
+            );
+            if (i < words.length - 1) {
+              sp.push(
+                <span className="underline" key={sp.length}>
+                  {' '}
+                </span>,
+              );
+            }
+          }
+        }
+        setShowPhon(sp);
+      }
+    }
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
-          </Link>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <NextUIProvider>
+      <main className="flex min-h-screen flex-col p-6">
+        <div className="flex w-full flex-wrap gap-4 md:flex-nowrap">
+          <Input
+            type="text"
+            label="Sentence"
+            ref={inputRef}
+            onKeyDown={handleKeyDown}
+          />
         </div>
-      </div>
+        <div className="mt-10">
+          <Card>
+            <CardBody>
+              <p>{showPhon}</p>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="mt-10">
+          <Card>
+            <CardBody>
+              <p>{showWord}</p>
+            </CardBody>
+          </Card>
+        </div>
+      </main>
+    </NextUIProvider>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
